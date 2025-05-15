728f2e8f698fc8b112d4e6c06f826f9de1c7575e
KushalManuh <Kushal.manuh@gmail.com>
2025-05-15 00:16:35 -0700

    Add files via upload

---
+++ Simple_dice_game.py
@@ -0,0 +1,58 @@
+import random
+dice=0
+sp1=0
+sp2=0
+print("Play player 1")
+while dice!=1:
+    choice=input("Would you like to Roll or Stop?: ").lower()
+    if choice=="roll":
+        dice=random.randint(1,6)
+        if dice==1:
+            print("Dice rolled 1...")
+            print("Player 1 score=0")
+            sp1=0
+            print()
+            break
+        else:
+            print("Dice rolled", dice)
+            sp1+=dice
+            print("Player 1 score=", sp1)
+    elif choice=="stop":
+        print("Player 1 score =",sp1)
+        print()
+        break
+    else:
+        print("Invalid Choice")
+        
+print("Play player 2")
+while dice!=1:
+    choice=input("Would you like to Roll or Stop?: ").lower()
+    if choice=="roll":
+        dice=random.randint(1,6)
+        if dice==1:
+            print("Dice rolled 1...")
+            print("Player 2 score=0")
+            sp2=0
+            print()
+            break
+        else:
+            print("Dice rolled", dice)
+            sp2+=dice
+            print("Player 2 score=", sp2)
+    elif choice=="stop":
+        print("Player 2 score =",sp2)
+        print()
+        break
+    else:
+        print("Invalid Choice")
+print("---------------------------------------------------------------------------")
+print("Results: ")
+print()
+print("Player 1 scored", sp1)
+print("Player 2 scored", sp2)
+if sp1>sp2:
+    print("Player 1 wins!")
+elif sp1<sp2:
+    print("Player 2 wins!")
+else:
+    print("I'ts a draw!")
